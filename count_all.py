c136179ec03018ef448a964644f4470b6ab7c2b7
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-12 21:31:40 +0500

    count_all

--- count_all.py
+++ count_all.py
@@ -1,10 +1,23 @@
 def count_all(txt):
     """
-    Given a function that takes a string and calculates the number of letters and digits within it.
+    Given a function that takes a string and calculates the number of 
+    letters and digits within it.
     Return the result in a dictionary.
     Args:
         txt(str): count letters and digits
     Returns:
         dict: dictionary with letters and digits
     """
-    return
+    text = {}
+    num = 0
+    lat = 0
+    for i in txt:
+        if txt.isdigit():
+            num += 1
+        if not(txt.isdigit()):
+            lat += 1
+    text["LATTERS"]= lat
+    text["DIGITS"] = num
+
+    return text
+print(count_all("Hello_World"))
